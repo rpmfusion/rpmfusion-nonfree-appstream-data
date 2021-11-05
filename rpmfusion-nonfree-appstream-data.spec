c8156753b9c03f993a4e24d8df9ac4cd02b0023b
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2021-11-05 10:43:48 +0000

    feat: regenerate

--- rpmfusion-nonfree-appstream-data.spec
+++ rpmfusion-nonfree-appstream-data.spec
@@ -3,7 +3,7 @@
 
 Name:       %{reponame}-%{repoversion}-appstream-data
 Version:    34
-Release:    2%{?dist}
+Release:    3%{?dist}
 Summary:    Appstream metadata for the RPM Fusion nonfree repository
 BuildArch:  noarch
 
@@ -39,6 +39,9 @@ DESTDIR=%{buildroot} appstream-util install %{SOURCE0} %{SOURCE1}
 %dir %{_datadir}/app-info/xmls
 
 %changelog
+* Fri Nov 05 2021 Ankur Sinha <ankursinha AT fedoraproject DOT org> - 34-3
+- Regenerate
+
 * Sun May 09 2021 Leigh Scott <leigh123linux@gmail.com> - 34-2
 - Regenerate
 
--- update-appdata-rpmfusion-nonfree.sh
+++ update-appdata-rpmfusion-nonfree.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 RELEASE=""
 URL=""
-URL1=""
+#URL1=""
 
 main ()
 {
     mkdir rpmfusion-nonfree/ -pv
     cd rpmfusion-nonfree || exit -1
 #
-    if [ "$RELEASE" =  "35" ]; then
+    if [ "$RELEASE" =  "36" ]; then
         URL="rsync://download1.rpmfusion.org/rpmfusion/nonfree/fedora/development/rawhide/Everything/x86_64/os/*"
-    elif [ "$RELEASE" = "34" ]; then
+    elif [ "$RELEASE" = "35" ]; then
+        URL="rsync://download1.rpmfusion.org/rpmfusion/nonfree/fedora/releases/35/Everything/x86_64/os/*"
+    elif [ "$RELEASE" =  "34" ]; then
         URL="rsync://download1.rpmfusion.org/rpmfusion/nonfree/fedora/releases/34/Everything/x86_64/os/*"
-        URL1="rsync://download1.rpmfusion.org/rpmfusion/nonfree/fedora/updates/34/x86_64/*"
-    elif [ "$RELEASE" =  "33" ]; then
-        URL="rsync://download1.rpmfusion.org/rpmfusion/nonfree/fedora/releases/33/Everything/x86_64/os/*"
         #URL1="rsync://download1.rpmfusion.org/rpmfusion/nonfree/fedora/updates/33/x86_64/*"
     fi
 
     rsync -avPh --delete "$URL" .
-    rsync -avPh --exclude debug "$URL1" ./Packages/
+#    rsync -avPh --delete --exclude debug "$URL1" ./Packages/
 
     rm -rf repo*
-    rm -rf Packages/repo*
-    createrepo -d Packages/
-    repomanage -o --space  ./Packages/ | xargs rm
-    rm -rf Packages/repo*
-
+#    rm -rf Packages/repo*
+#    createrepo -d Packages/
+#    repomanage -o --space  ./Packages/ | xargs rm
+#    rm -rf Packages/repo*
+#
     appstream-builder --verbose  --include-failed --max-threads=6 --log-dir=./logs/ \
     --packages-dir=./Packages/ --temp-dir=./tmp/ --output-dir=./appstream-data/ \
     --basename="rpmfusion-nonfree-$RELEASE" --origin="rpmfusion-nonfree-$RELEASE" \
@@ -40,7 +39,7 @@ usage ()
     echo "$0 -r <release>"
     echo "- update appdata for rpmfusion nonfree repository"
     echo "options:"
-    echo "-r <release> one of 33, 34 and 35"
+    echo "-r <release> one of 34, 35 and 36"
 }
 
 
